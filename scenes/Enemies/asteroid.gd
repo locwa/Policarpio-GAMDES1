a25a797db179686f54d28d066de5cac73bccdb24
Louis Policarpio <94048557+locwa@users.noreply.github.com>
2025-08-30 10:47:47 +0800

    initial commit

---
+++ scenes/Enemies/asteroid.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# Function when the node enters the scenetree for the first time
+func _ready():
+	# Starting position
+	position = Vector2(randf_range(50, 300),0)
+	# Ship Scale
+	scale = Vector2(1, 1)
+
+# Called every singe frame 'delta' time account for frame rate diff
+# It is the elapsed time since the previous frame
+func _process(delta):
+	position += Vector2(0, .75) * 60 * delta
+	rotation += (.5) * 3 * delta
+	
---
+++ scenes/asteroid_boundary.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("hello")
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.

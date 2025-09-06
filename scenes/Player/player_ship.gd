fe294f34dfcb802955fd51d57ace9f14da02253c
Louis Policarpio <94048557+locwa@users.noreply.github.com>
2025-09-06 08:30:58 +0800

    added borders and changed player ship to characterbody2d

--- scenes/Player/player_ship.gd
+++ scenes/Player/player_ship.gd
@@ -1,29 +1,16 @@
-extends Node2D
+extends CharacterBody2D
 
-# Function when the node enters the scenetree for the first time
+@export var speed := 300
+#called when the node enters the scentree for the first time
 func _ready():
-	# Starting position
-	position = Vector2(200,570)
+	#starting position
+	position = Vector2(200,500)
+	# ship scale
+	scale =Vector2(.50,.50)
+	# Called every single frame. 'delta' time account for frame rate differences
 	
-	# Ship Scale
-	scale = Vector2(1, 1)
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("enemy"):
-		print("hit")
-
-# Called every singe frame 'delta' time account for frame rate diff
-# It is the elapsed time since the previous frame
+# it is the elapsed time since the previous frame
 func _process(delta):
-	if Input.is_action_pressed("move_right"):
-		if self.position.x < 397:
-			position+= Vector2(1, 0) * 120 * delta
-	if Input.is_action_pressed("move_left"):
-		if self.position.x > 3:
-			position+= Vector2(-1, 0) * 120 * delta
-	if Input.is_action_pressed("move_up"):
-		if self.position.y  >= 10:
-			position+= Vector2(0, -1) * 120 * delta
-	if Input.is_action_pressed("move_down"):
-		if self.position.y <= 590:
-			position+= Vector2(0, 1) * 120 * delta
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * speed
+	move_and_slide()
